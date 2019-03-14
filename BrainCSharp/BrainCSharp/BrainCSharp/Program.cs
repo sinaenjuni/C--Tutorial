46767b55e6468144e9c46cf1c883fe8ec0a984f5
Hyeonjun Sin <40359803+sinaenjuni@users.noreply.github.com>
2019-03-14 11:01:09 +0900

    1
    1

---
+++ BrainCSharp/BrainCSharp/BrainCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if(args.Length == 0)
+            {
+                Console.WriteLine("사용법: HelloWorld.exe <이름>");
+                return;
+            }
+
+            Console.WriteLine("Hello, {0}, {1}, {2}", args[0], args[2], args[1]);
+        }
+    }
+}
